6165ec5ce4bb9945a79089f87fd8088e8e548c96
Anand Rajagopal <anand_rajagopal@ischool.berkeley.edu>
2016-02-05 16:10:11 -0800

    changed tabs to spaces

--- Gadfly.py
+++ Gadfly.py
@@ -3,18 +3,18 @@ from enum import Enum
 QuestionTypes = Enum('GapFill')
 
 class Question:
-	def __init__(self):
-		self._type = QuestionTypes.GapFill
+    def __init__(self):
+        self._type = QuestionTypes.GapFill
 
-	def setQuestion(text):
-		self._question = text
+    def setQuestion(text):
+        self._question = text
 
-	def setAnswer(text):
-		self._answer = text
+    def setAnswer(text):
+        self._answer = text
 
-	def getQuestion():
-		return self._question
+    def getQuestion():
+        return self._question
 
-	def getAnswer():
-		return self._answer
-	
+    def getAnswer():
+        return self._answer
+    
