6bb21ded7e7c0a12cf57a6c9195eabc2be5e48a6
Anand Rajagopal <anand_rajagopal@ischool.berkeley.edu>
2016-02-05 16:08:06 -0800

    Refactored code into 2 classes

--- vandalizer.py
+++ vandalizer.py
@@ -20,25 +20,17 @@ class SourceText(object):
         transformations, selections, and question generation functions.
         """
         self.file = file
-        self.raw = self.load_text()
-        print("Initializing: Text loaded...")
-
-        self.pos = self.store_pos()
-        print("Initializing: POS complete...")
-
-        self.derived_sents = self.pos
-        # This is using the terminology from Heilman and Smith (2010)
-        # For now this does nothing special. Should later simplify sentences
-        # in various ways. Likely more derived sentences than in source text
-        print("Initializing: Deriving Sentences complete...")
-
-        self.selected_sents = self.select_sents()
-        # ? - not sure if this should happen before or after above
-        # likely depends on computational cost (?)
-        print("Initializing: Selecting Sentences complete...")
-
-        self.questions = self.create_questions()
-        print("Initializing: Creating Questions complete...")
+        print("Reading file "+self.file)
+        self.raw_text = self.load_text()
+        print("Initializing: Text loaded.")
+        self.pos_tagged_sents = self.store_pos()
+        print("Initializing: POS Tagging complete.")
+        #This is currently not being used anywhere
+        # self._source_obj.derived_sents = self.pos_tagged_sents
+        # # This is using the terminology from Heilman and Smith (2010)
+        # # For now this does nothing special. Should later simplify sentences
+        # # in various ways. Likely more derived sentences than in source text
+        # print("Initializing: Deriving Sentences complete...")
 
     def load_text(self):
         f = open(self.file, encoding='utf-8')
@@ -46,7 +38,7 @@ class SourceText(object):
 
     def store_pos(self):
         pos_lst = []
-        for line in self.raw:
+        for line in self.raw_text:
             line = line.strip()
             if len(line) == 1:
                 continue  # b/c readlines has empty lines for now
@@ -56,31 +48,17 @@ class SourceText(object):
                 pos_lst.append(nltk.pos_tag(tokenized))
         return pos_lst
 
-    def select_sents(self):
-        """ Later: Select by some notion of a good sentence.
-        Soon: Select by not having annoying anaphora, etc.
-        Now: Select by not having PRP or PRP$.
-        """
-        selected_sent_lst = []
-        count_bad = 0
-        for sent in self.pos:
-            flag = False  # Sorry vijay
-            for token, pos in sent:
-                if pos in ["PRP", "PRP$"]:
-                    count_bad += 1
-                    flag = True
-                    break
-            if flag is False:
-                selected_sent_lst.append(sent)
-        # This is garbage metrics for now:
-        print("Bad sentences = {}".format(count_bad))
-        print("Good sentences = {}".format(len(self.pos)-count_bad))
-        return selected_sent_lst
+    
 
-    def create_questions(self):
+class GapFillGenerator:
+
+    def __init__(self, source_obj):
+        self._source_obj = source_obj
+
+    def question_generation(self,selected_sents):
         """ Remove blank and display question"""
         possible_questions = []
-        for sent in self.selected_sents:
+        for sent in selected_sents:
             last_n = -2
             last_answer = ""
             last_temp_sent = ""
@@ -102,27 +80,68 @@ class SourceText(object):
                     last_temp_sent = temp_sent
                     last_n = n
         return possible_questions
+        
 
-    def print_questions(self):
+    def sentence_simplification(self):
+        pass
+
+    def sentence_selection(self):
+        """ Later: Select by some notion of a good sentence.
+        Soon: Select by not having annoying anaphora, etc.
+        Now: Select by not having PRP or PRP$.
+        """
+        selected_sent_lst = []
+        count_bad = 0
+        for sent in self._source_obj.pos_tagged_sents:
+            flag = False  # Sorry vijay
+            for token, pos in sent:
+                if pos in ["PRP", "PRP$"]:
+                    count_bad += 1
+                    flag = True
+                    break
+            if flag is False:
+                selected_sent_lst.append(sent)
+        # This is garbage metrics for now:
+        print("Bad sentences = {}".format(count_bad))
+        print("Good sentences = {}".format(len(self._source_obj.pos_tagged_sents)-count_bad))
+        return selected_sent_lst
+
+    def print_questions(self, outputFile):
         for n, (question, answer) in enumerate(self.questions):
-            print("\nQuestion #{}".format(n))
-            print("Q: {}".format(question.encode('ascii', 'ignore')))
-            print("A: {}".format(answer.encode('ascii', 'ignore')))
+            outputFile.write("\nQuestion #{}\n".format(n+1))
+            outputFile.write(
+                "Q: {}".format(question.encode('ascii', 'ignore'))
+                )
+            outputFile.write(
+                "A: {}\n".format(answer.encode('ascii', 'ignore'))
+                )
+        outputFile.write("")
+
+    def run(self):
+        #This step of filtering can alternatively happen at the level of filterning questions
+        self.selected_sents = self.sentence_selection()
+        print("Initializing: Sentence Selection complete.")
+
+        self.questions = self.question_generation(self.selected_sents)
+        print("Initializing: Question generation complete.")
 
     def question_count(self):
         return len(self.questions)
 
 ##############################################################################
 
-
 def main():
-    nltk.download(['punkt', 'averaged_perceptron_tagger'])
+    #This needs to go into a setup file.
+    #nltk.download(['punkt', 'averaged_perceptron_tagger'])
     news_articles = os.path.join(NEWS_ARTICLES_DIR, "*.txt")
+    outputFile = open("output.txt","w")
     for file in glob.glob(news_articles):
         article = SourceText(file)
+        generator = GapFillGenerator(article)
+        generator.run()
+        generator.print_questions(outputFile)
         print("This found {} questions from the text.".format(
-            article.question_count()))
-        article.print_questions()
+            generator.question_count()))
 
 ##############################################################################
 
