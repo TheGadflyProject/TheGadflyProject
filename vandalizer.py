08b4f262e63ba8d8c6500fdf8d6cefc9737f0713
vijayv <acchi369@gmail.com>
2016-02-08 21:10:27 +0000

    create setup file

--- vandalizer.py
+++ vandalizer.py
@@ -1,7 +1,6 @@
 # !/usr/bin/python3
 
 # Imports
-import nltk
 import glob
 import os
 from gap_fill_generator import GapFillGenerator
@@ -16,8 +15,6 @@ NEWS_ARTICLES_DIR = os.path.join(PROJECT_DIR, "news_articles")
 
 
 def main():
-    # This needs to go into a setup file.
-    nltk.download(['punkt', 'averaged_perceptron_tagger'])
     news_articles = os.path.join(NEWS_ARTICLES_DIR, "*.txt")
     output_file = open("output.txt", "w")
     for file in glob.glob(news_articles):
