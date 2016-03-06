8191fee6412b773111a8f0cd589ce7a83b123f27
= <=>
2016-03-06 02:58:19 +0000

    SourceText to accept raw text as input

--- vandalizer.py
+++ vandalizer.py
@@ -11,23 +11,19 @@ from gadfly.source_text import SourceText
 PROJECT_DIR = os.path.dirname(__file__)
 NEWS_ARTICLES_DIR = os.path.join(PROJECT_DIR, "news_articles")
 
-##############################################################################
-
 
 def main():
     news_articles = os.path.join(NEWS_ARTICLES_DIR, "*.txt")
     output_file = open("output.txt", "w")
-    for file in glob.glob(news_articles):
-        article = SourceText(file)
+    for file_name in glob.glob(news_articles):
+        f = open(file_name, encoding='utf-8')
+        article = SourceText(f.readlines())
         generator = GapFillGenerator(article)
         generator.run()
         generator.output_questions_to_file(output_file)
         print("This found {} questions from the text.\n".format(
             generator.question_count()))
         generator._print_source_sentences()
-
-##############################################################################
-
 
 if __name__ == '__main__':
     main()
