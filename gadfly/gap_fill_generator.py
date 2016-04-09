dcb843275a1aa67b7e16a686c8561407f00f4811
Anand Rajagopal <anand_rajagopal@ischool.berkeley.edu>
2016-04-08 17:09:09 -0700

    INitial MCQ

--- gadfly/gap_fill_generator.py
+++ gadfly/gap_fill_generator.py
@@ -4,11 +4,11 @@ from .sentence_summarizer import TF_IDFSummarizer
 from .transducer import Transducer
 from spacy.tokens.token import Token
 from enum import Enum
-from itertools import product
 import string
-import re
 import types
-from enum import Enum
+import collections
+import numpy
+
 
 def tfidf(self):
     selector = TF_IDFSummarizer(EDA=True)
@@ -20,6 +20,7 @@ def tfidf(self):
         print(sent)
     print()
     return sentences
+
 
 class QuestionType(Enum):
     gap_fill = "gap_fill"
@@ -59,21 +60,77 @@ class GapFillGenerator:
 
     def gen_named_entity_blanks(self):
         named_entity_questions = []
+        entities_dict = self.build_entities_dictionary()
+        print (entities_dict)
         entities = self.find_named_entities()
         for sent in self._top_sents:
             sent_text = "".join(
                     [t.text_with_ws if type(t) == Token else t for t in sent])
             for ent in entities:
                 if (sent.start < ent.start and sent.end > ent.end):
-                    gap_fill_question = str(self._parsed_text[sent.start:ent.start]) \
-                                        + self._GAP + str(self._parsed_text[ent.end:sent.end])
+                    gap_fill_question = str(self._parsed_text[
+                                            sent.start:ent.start]) \
+                                        + self._GAP + str(self._parsed_text[
+                                                          ent.end:sent.end])
+                    other_choices = self.generate_other_choices(entities_dict,
+                                                                ent, sent)
+                    print ("MCQ")
+                    print (gap_fill_question)
+                    print(ent)
+                    print (other_choices)
+                    print()
                     question = Question(sent_text, gap_fill_question,
                                         ent, QuestionType.gap_fill,
                                         GapFillBlankType.named_entities)
                     named_entity_questions.append(question)
-
         return named_entity_questions
-        
+
+    def generate_other_choices(self, entities_dict, entity, sent):
+        all_entities = entities_dict[entity.label_]
+        other_choices = [(ent,
+                          str(numpy.round(numpy.array(
+                                          [ent.similarity(entity)]),
+                                          3))
+                          ) for ent in all_entities]
+        # remove choices that are already in the sentence
+
+        other_choices = [(span, sim) for span, sim in other_choices if
+                         entity.text_with_ws.strip() not in
+                         span.text_with_ws.strip() and
+                         span.text_with_ws not in sent.text_with_ws]
+        other_choices = self.trim_choices(other_choices)
+        other_choices = sorted(other_choices, key=lambda x: x[1], reverse=True)
+        return other_choices[:3]
+
+    def build_entities_dictionary(self):
+        entities_dict = collections.defaultdict(list)
+        entities = self.find_named_entities()
+        for entity in entities:
+            if entity.text_with_ws.strip() not in [e.text_with_ws.strip()
+               for e in entities_dict[entity.label_]]:
+                    entities_dict[entity.label_].append(entity)
+        return entities_dict
+
+    def trim_choices(self, other_choices):
+        ents = [word.text_with_ws.strip() for word, sim in other_choices]
+        ents = sorted(ents, key=len)
+        keep_list = []
+        i = 0
+        while i < len(ents):
+            j = i+1
+            kill = False
+            while j < len(ents):
+                if ents[i] in ents[j]:
+                    kill = True
+                    break
+                j += 1
+            if kill is False:
+                keep_list.append(ents[i])
+            i += 1
+        ents = [(ent, sim) for ent, sim in other_choices
+                if ent.text_with_ws.strip() in keep_list]
+        return ents
+
     def generate_questions(self):
         question_set = []
         for gap_type in self._gap_types:
