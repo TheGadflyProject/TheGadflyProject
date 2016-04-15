37cc70c76493dc0a5d7eded433a78479fb1d8b23
= <=>
2016-04-15 19:31:41 +0000

    better check for spacy data

--- gadfly/spacy_singleton.py
+++ gadfly/spacy_singleton.py
@@ -20,7 +20,16 @@ if not os.path.isdir(data_path):
 
 def _spacy_en():
     yield None
-    spacyen = spacy.load('en_default', via=data_path)
+    try:
+        spacyen = spacy.load('en_default', via=data_path)
+    except RuntimeError as e:
+        if e.message == "Model not installed. Please run 'python -m spacy.en.download' to install latest compatible model.":
+            print("Need to download Spacy data. Starting download now")
+            sputnik.install('spacy', spacy.about.__version__,
+                            'en_default', data_path=data_path)
+            spacyen = spacy.load('en_default', via=data_path)
+        else:
+            raise
     while True:
         yield spacyen
 
