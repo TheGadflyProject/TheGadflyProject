dff7867b7f6ae1d3dc5ccb6d684111eae8b9d0eb
= <=>
2016-05-01 01:58:43 +0000

    add test for transducer

--- test/test_transducer.py
+++ test/test_transducer.py
@@ -6,9 +6,18 @@ from spacy.tokens.token import Token
 
 class TransducerTest(unittest.TestCase):
 
-    def test_instanstiated_object_of_correct_type(self):
+    def test_transuder_returns_expected_putin_sentence(self):
         sent = """Prime Minister Putin, country's paramount leader, cut short a trip to Siberia."""
         correct_output = "Prime Minister Putin cut short a trip to Siberia."
+        parsed = spacy_singleton.spacy_en()(sent)
+        transduced = Transducer.transduce(parsed)
+        transduced = "".join(
+            [t.text_with_ws if type(t) == Token else t for t in transduced])
+        return self.assertEqual(transduced, correct_output)
+        
+    def test_transuder_returns_expected_sentence(self):
+        sent = """Johnathan Wilson, a shy man, replaced his position in the dark."""
+        correct_output = "Johnathan Wilsonreplaced his position in the dark."
         parsed = spacy_singleton.spacy_en()(sent)
         transduced = Transducer.transduce(parsed)
         transduced = "".join(
